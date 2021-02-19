a65e2d229567e21102058b2314b6d7d09a9ab25b
siandrev <andreev.sergey.i@yandex.ru>
2021-02-19 14:39:14 +0300

    Instant trades uniswap support added

--- src/bots/instant-trade-bot.js
+++ src/bots/instant-trade-bot.js
@@ -4,6 +4,8 @@ import Bot from './bot';
 class InstantTradeBot extends Bot {
 
     bulletEmoji = '\ud83d\udfe2';
+    uniSwapEmoji = '\ud83e\udd84';
+    oneInchEmoji = '\ud83d\udc34'
 
     chatId = process.env.INSTANT_TRADE_CHAT_ID;
 
@@ -12,16 +14,24 @@ class InstantTradeBot extends Bot {
     }
 
     async sendNotification(request) {
-        const { walletAddress, txHash, amountFrom, amountTo, symbolFrom, symbolTo } = request;
+        let { provider, walletAddress, txHash, amountFrom, amountTo, symbolFrom, symbolTo } = request;
 
         const scannerAddressUrl = networks[0].scannerAddressBaseUrl + walletAddress;
         const scannerTxUrl = networks[0].scannerTxBaseUrl + txHash;
 
         const priceInfo = await coinGeckoApi.getAllPrices(symbolFrom.toLowerCase());
+        let providerEmoji = this.oneInchEmoji;
+
+        if (provider === 'UniSwap') {
+            providerEmoji = this.uniSwapEmoji;
+        } else {
+            provider = 'OneInch';
+        }
 
         const message = `
 New instant trade swap was created by
 <a href="${scannerAddressUrl}">\ud83d\udcf6 ${walletAddress}</a> 
+with <b>${provider}</b> ${providerEmoji}
 
 ${priceInfo.ethPrice ? this.getFormattedBullets(priceInfo.ethPrice * amountFrom) : ''}
 
